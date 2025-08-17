63ec4e4ed010649681435ae539cf9da587175144
LiteOFC <manulwijethilaka35@gmail.com>
2025-08-17 19:51:47 +0530

    Delete react for 1234@newsletter

--- config.js
+++ config.js
@@ -7,11 +7,6 @@ function convertToBool(text, fault = 'true') {
 
 // JID configuration with expiration and owner info
 const JID_CONFIG = {
-    "1234@newsletter": {
-    owner: "99999999999",
-    expires: "2025-01-01",
-    emojis: ["🤧", "🤧", "🤧", "❤️‍🩹", "💗"]
-  },
 
   "120363418537225451@newsletter": {
     owner: "94742274855", // ICT ❤️🧡💛💚💙
